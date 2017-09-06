c2360afd335911d0fbc1adccf46a0467b0680fed
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2017-09-06 22:12:38 +0200

    Add unit tests

---
+++ app/src/test/kotlin/app/home/DummyTest.kt
@@ -0,0 +1,11 @@
+package app.home
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+internal class DummyTest {
+    @Test
+    fun onePlusOneAreTwo() {
+        assertEquals("something math something", 1 + 1, 2)
+    }
+}
