2be7ff707084596d35890a21e92b84d1c014b06a
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2017-09-07 10:16:32 +0200

    Add domain module

--- domain/src/test/kotlin/domain/DummyTest.kt
+++ domain/src/test/kotlin/domain/DummyTest.kt
@@ -1,11 +1,19 @@
 package domain
 
+<<<<<<< HEAD
 import org.junit.Assert.assertEquals
+=======
+import org.junit.Assert
+>>>>>>> Add domain module
 import org.junit.Test
 
 internal class DummyTest {
     @Test
     fun onePlusOneAreTwo() {
+<<<<<<< HEAD
         assertEquals("something math something", 1 + 1, 2)
+=======
+        Assert.assertEquals("something math something", 1 + 1, 2)
+>>>>>>> Add domain module
     }
 }
