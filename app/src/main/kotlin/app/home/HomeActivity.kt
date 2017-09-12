0da70c450450ba8726e1c871b072f8ecb1a010f6
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2017-09-12 12:05:35 +0200

    Add activity layout

--- app/src/main/kotlin/app/home/HomeActivity.kt
+++ app/src/main/kotlin/app/home/HomeActivity.kt
@@ -1,5 +1,12 @@
 package app.home
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import jorge.template.blank.R
 
-internal class HomeActivity : AppCompatActivity()
+internal class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
